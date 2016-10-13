50f9596f97e6ede0b5861d519f9f1a9ba7a54ad2
Noah <Noah@DESKTOP-IISV8S8>
2016-10-12 22:42:26 -0700

    Adding the Swashbuckle Nuget Package to the project. Swashbuckle implements Swagger. Swagger metadata will be added to our Web API to guide the REST Client auto generation process.

--- Anchovy/Anchovy.API.Service/Models/Pizza.cs
+++ Anchovy/Anchovy.API.Service/Models/Pizza.cs
@@ -4,7 +4,7 @@ namespace Anchovy.API.Service.Models
 {
     public class Pizza
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public Size Size { get; set; }
         public Crust Crust { get; set; }
         public Sauce Sauce { get; set; }
