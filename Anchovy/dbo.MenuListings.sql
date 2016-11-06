3cd8354644d6c24823726b35793b806d14ab2aa1
Kevari <Kevari@KEVARI-PC>
2016-11-06 19:34:05 -0400

    Gui work

---
+++ Anchovy/dbo.MenuListings.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MenuListings] (
+    [Id]   INT             IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX)  NULL,
+    [Cost] DECIMAL (18, 2) NOT NULL,
+    [SizeId] INT NULL, 
+    CONSTRAINT [PK_dbo.MenuListings] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Anchovy/dbo.Sizes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Sizes] (
+    [Id]   INT             IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX)  NULL,
+    [Cost] DECIMAL (18, 2) NOT NULL,
+    [SizeId] INT NOT NULL, 
+    CONSTRAINT [PK_dbo.Sizes] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
