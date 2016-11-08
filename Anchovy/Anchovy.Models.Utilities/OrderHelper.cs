990c89eba31af10621ede73cc80e62b8261b5915
Noah <Noah@DESKTOP-IISV8S8>
2016-11-08 15:39:08 -0600

    updating reference in utilities dll to client models (since these helper functions will be used on the client side). added more helper methods, especially in order. began work on database seed.

--- Anchovy/Anchovy.Models.Utilities/OrderHelper.cs
+++ Anchovy/Anchovy.Models.Utilities/OrderHelper.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Anchovy.API.Client.Models;
 using Anchovy.API.Service.Models;
+using Cook = Anchovy.API.Client.Models.Cook;
+using Customer = Anchovy.API.Client.Models.Customer;
+using Order = Anchovy.API.Client.Models.Order;
 
 namespace Anchovy.Models.Utilities
 {
     public class OrderHelper
     {
-        public virtual decimal CalculateOrderCost(Order order)
+        public virtual double? CalculateOrderCost(Order order)
         {
             var lineHelper = new LineHelper();
             return order.Lines.Sum(line => lineHelper.CalculateLineCost(line));
+        }
+
+        public virtual IList<Order> GetOrdersByStatus(IList<Order> orders, OrderStatus orderStatus)
+        {
+            return orders.Where(_ => _.OrderStatus.Equals(orderStatus)).ToList();
+        }
+
+        public virtual IList<Order> GetCookOrders(IList<Order> orders, Cook cook)
+        {
+            return orders.Where(_ => _.CookId.Equals(cook.Id)).ToList();
+        }
+
+        public virtual IList<Order> GetCookOrdersByStatus(IList<Order> orders, Cook cook, OrderStatus orderStatus)
+        {
+            return orders.Where(_ => _.CookId.Equals(cook.Id) && _.OrderStatus.Equals(orderStatus)).ToList();
+        }
+
+        public virtual IList<Order> GetCustomerOrders(IList<Order> orders, Customer customer)
+        {
+            return orders.Where(_ => _.CustomerId.Equals(customer.Id)).ToList();
+        }
+        public virtual IList<Order> GetCustomerOrdersByStatus(IList<Order> orders, Customer customer, OrderStatus orderStatus)
+        {
+            return orders.Where(_ => _.CustomerId.Equals(customer.Id) && _.OrderStatus.Equals(orderStatus)).ToList();
         }
     }
 }
